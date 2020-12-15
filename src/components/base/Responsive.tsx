d021ebc44393ab667f2ffdc056067881f6266c06
yuyaebean <yuyaebean@gmail.com>
2020-12-16 01:55:49 +0900

    Feat: Make Header component responsive

---
+++ src/components/base/Responsive.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+import { mediaQuery } from 'lib/media';
+
+interface ResponsiveProps {
+  children: React.ReactNode;
+  [rest: string]: any;
+}
+
+const Block = styled.div`
+  width: 1728px;
+  margin: 0 auto;
+
+  ${mediaQuery(1919)} {
+    width: 1376px;
+  }
+
+  ${mediaQuery(1440)} {
+    width: 1024px;
+  }
+
+  ${mediaQuery(1056)} {
+    width: calc(100% - 2rem);
+  }
+`;
+
+function Responsive({ children, ...rest }: ResponsiveProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Block {...rest}>{children}</Block>;
+}
+
+export default Responsive;
