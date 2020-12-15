0d03edde734872a4408dd718d3c227370bdad0fd
yuyaebean <yuyaebean@gmail.com>
2020-12-16 01:55:49 +0900

    Feat: Make Header component responsive

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,27 +1,25 @@
 import styled from '@emotion/styled';
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-const HeaderBlock = styled.header`
+import Responsive from 'components/base/Responsive';
+
+const Block = styled.header`
   position: sticky;
   top: 0;
   left: 0;
   background-color: #212121;
   color: #fafafa;
   width: 100%;
-  height: ${({ theme }) => theme.constants.headerHeight};
 `;
 
-const HeaderList = styled.ul`
+const Inner = styled(Responsive)`
   display: flex;
   align-items: center;
   list-style: none;
-  height: 100%;
-  padding: 0 1.5rem;
-  margin: 0;
+  height: ${({ theme }) => theme.constants.headerHeight};
 `;
 
-const HeaderListItem = styled.li``;
+const Left = styled.div``;
 
 const Logo = styled.img`
   /* align-self는 safari에서 깨지는거 방지용 */
@@ -30,21 +28,21 @@ const Logo = styled.img`
   height: auto;
 `;
 
-const Header: React.FC = () => {
+function Header() {
   return (
-    <HeaderBlock>
-      <HeaderList>
-        <HeaderListItem>
+    <Block>
+      <Inner>
+        <Left>
           <Link to="/">
             <Logo
               src="https://mju-likelion.s3.ap-northeast-2.amazonaws.com/static/basiclogo_E_H_W.png"
               alt="MJU Likelion Logo"
             />
           </Link>
-        </HeaderListItem>
-      </HeaderList>
-    </HeaderBlock>
+        </Left>
+      </Inner>
+    </Block>
   );
-};
+}
 
 export default Header;
