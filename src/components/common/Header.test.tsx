61589e59e652ab6fa8bdc5a5d5b4b8fb398378e5
soultree-fly <yuyaebean@gmail.com>
2021-01-17 14:06:42 +0900

    Test: Add RecoilRoot for test

--- src/components/common/Header.test.tsx
+++ src/components/common/Header.test.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 import theme from 'theme';
 
@@ -11,11 +12,14 @@ jest.mock('../../ModalPortal', () => () => <div id='modal' />);
 describe('<Header />', () => {
   it('has a logo', () => {
     const header = render(
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Header />
-        </ThemeProvider>
-      </BrowserRouter>,
+      <RecoilRoot>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <Header />
+          </ThemeProvider>
+        </BrowserRouter>
+        ,
+      </RecoilRoot>,
     );
 
     const logo = header.getByAltText('MJU Likelion Logo');
@@ -24,11 +28,14 @@ describe('<Header />', () => {
 
   it('has a login button', () => {
     const header = render(
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Header />
-        </ThemeProvider>
-      </BrowserRouter>,
+      <RecoilRoot>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <Header />
+          </ThemeProvider>
+        </BrowserRouter>
+        ,
+      </RecoilRoot>,
     );
 
     const loginButton = header.getByText('로그인');
@@ -37,11 +44,14 @@ describe('<Header />', () => {
 
   it('has a signin button', () => {
     const header = render(
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <Header />
-        </ThemeProvider>
-      </BrowserRouter>,
+      <RecoilRoot>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <Header />
+          </ThemeProvider>
+        </BrowserRouter>
+        ,
+      </RecoilRoot>,
     );
 
     const loginButton = header.getByText('회원가입');
